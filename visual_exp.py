94c6a8a76840be1e07e4fa047656e220987efd0e
kagort <kagort@yandex.ru>
2024-09-30 19:31:45 +0300

    Good

--- visual_exp.py
+++ visual_exp.py
@@ -34,30 +34,17 @@ Noun = {word: [] for word in words}
 Participle = {word: [] for word in words}
 word_counts = {word: 0 for word in words}
 
-# Нумерация предложений
-print("Список предложений в тексте:")
-for i, sent in enumerate(doc.sents, 1):
-    print(f"{i}. {sent}")
-
-print("\nНачало анализа...\n")
-
-# Выборка глаголов, существительных, прилагательных и причастий по каждому поисковому термину
+# Анализ зависимостей
 for word in words:
-    print(f"\nАнализ для термина: {word}\n")
     for sent in doc.sents:
-        print(sent)
         for token in sent:
             if token.lemma_ == word:
-                a = token
                 word_counts[word] += 1
-
-                if a.dep_ == 'nsubj':  # как подлежащее
-                    Sub[word].append(a.head.text)
-                if a.dep_ in ('dobj', 'obj', 'obl'):  # как дополнение
-                    Obj[word].append(a.head.text)
-
-                # Зависимые слова
-                for ch in a.children:
+                if token.dep_ == 'nsubj':
+                    Sub[word].append(token.head.text)
+                if token.dep_ in ('dobj', 'obj', 'obl'):
+                    Obj[word].append(token.head.text)
+                for ch in token.children:
                     if ch.pos_ == 'ADJ':
                         Adj[word].append(ch.lemma_)
                     elif ch.pos_ == 'NOUN':
@@ -65,26 +52,15 @@ for word in words:
                     elif ch.pos_ == 'VERB':
                         Participle[word].append(ch.text)
 
-# Лемматизация списков Sub и Obj
+# Лемматизация и фильтрация
+stop_w = stopwords.words("russian")
 lemmatized_Sub = {word: [" ".join([token.lemma_ for token in nlp(text)]) for text in Sub[word]] for word in words}
 lemmatized_Obj = {word: [" ".join([token.lemma_ for token in nlp(text)]) for text in Obj[word]] for word in words}
-
-# Работа со стоп-словами
-stop_w = stopwords.words("russian")
-stop_w.extend(['�', '-', '―', '”', 'запах', 'вкус'])
-
 filt_Sub = {word: [w for w in lemmatized_Sub[word] if w not in stop_w] for word in words}
 filt_Obj = {word: [w for w in lemmatized_Obj[word] if w not in stop_w] for word in words}
 filt_Adj = {word: [w for w in Adj[word] if w not in stop_w] for word in words}
 filt_Noun = {word: [w for w in Noun[word] if w not in stop_w] for word in words}
 filt_Participle = {word: [w for w in Participle[word] if w not in stop_w] for word in words}
-
-# Удаление повторов и сортировка
-sub = {word: sorted(list(set(filt_Sub[word]))) for word in words}
-obj = {word: sorted(list(set(filt_Obj[word]))) for word in words}
-adj = {word: sorted(list(set(filt_Adj[word]))) for word in words}
-noun = {word: sorted(list(set(filt_Noun[word]))) for word in words}
-participle = {word: sorted(list(set(filt_Participle[word]))) for word in words}
 
 # Частотные слова для контекстных окон
 fdist_sw_Sub = {word: FreqDist(filt_Sub[word]) for word in words}
@@ -95,10 +71,8 @@ fdist_sw_Participle = {word: FreqDist(filt_Participle[word]) for word in words}
 
 # Самые частотные слова для контекстных окон
 tooltips = {
-    'Глаголы подлежащего': [', '.join([f'{w[0]} ({w[1]})' for w in fdist_sw_Sub[word].most_common(7)]) for word in
-                            words],
-    'Глаголы дополнения': [', '.join([f'{w[0]} ({w[1]})' for w in fdist_sw_Obj[word].most_common(7)]) for word in
-                           words],
+    'Глаголы подлежащего': [', '.join([f'{w[0]} ({w[1]})' for w in fdist_sw_Sub[word].most_common(7)]) for word in words],
+    'Глаголы дополнения': [', '.join([f'{w[0]} ({w[1]})' for w in fdist_sw_Obj[word].most_common(7)]) for word in words],
     'Прилагательные': [', '.join([f'{w[0]} ({w[1]})' for w in fdist_sw_Adj[word].most_common(7)]) for word in words],
     'Существительные': [', '.join([f'{w[0]} ({w[1]})' for w in fdist_sw_Noun[word].most_common(7)]) for word in words],
     'Причастия': [', '.join([f'{w[0]} ({w[1]})' for w in fdist_sw_Participle[word].most_common(7)]) for word in words],
@@ -107,64 +81,76 @@ tooltips = {
 # Dash приложение
 app = dash.Dash(__name__)
 
-
 # Создаем график с Plotly
 def create_figure():
+    # Обновляем ключевые слова с частотой
+    words_with_counts = [f"{word} [{word_counts[word]}]" for word in words]
+
     fig = go.Figure()
 
     # Глаголы подлежащего
     fig.add_trace(go.Bar(
-        x=words,
+        x=words_with_counts,
         y=[len(Sub[word]) for word in words],
         name='Глаголы подлежащего',
         marker_color='green',
         customdata=[filt_Sub[word] for word in words],
         hovertext=tooltips['Глаголы подлежащего'],
-        hovertemplate='%{hovertext}<extra></extra>'
+        hovertemplate='%{hovertext}<extra></extra>',
+        text=[len(Sub[word]) for word in words],  # Число над столбцом
+        textposition='auto'
     ))
 
     # Глаголы дополнения
     fig.add_trace(go.Bar(
-        x=words,
+        x=words_with_counts,
         y=[len(Obj[word]) for word in words],
         name='Глаголы дополнения',
         marker_color='cyan',
         customdata=[filt_Obj[word] for word in words],
         hovertext=tooltips['Глаголы дополнения'],
-        hovertemplate='%{hovertext}<extra></extra>'
+        hovertemplate='%{hovertext}<extra></extra>',
+        text=[len(Obj[word]) for word in words],  # Число над столбцом
+        textposition='auto'
     ))
 
     # Прилагательные
     fig.add_trace(go.Bar(
-        x=words,
+        x=words_with_counts,
         y=[len(Adj[word]) for word in words],
         name='Прилагательные',
         marker_color='magenta',
         customdata=[filt_Adj[word] for word in words],
         hovertext=tooltips['Прилагательные'],
-        hovertemplate='%{hovertext}<extra></extra>'
+        hovertemplate='%{hovertext}<extra></extra>',
+        text=[len(Adj[word]) for word in words],  # Число над столбцом
+        textposition='auto'
     ))
 
     # Существительные
     fig.add_trace(go.Bar(
-        x=words,
+        x=words_with_counts,
         y=[len(Noun[word]) for word in words],
         name='Существительные',
         marker_color='red',
         customdata=[filt_Noun[word] for word in words],
         hovertext=tooltips['Существительные'],
-        hovertemplate='%{hovertext}<extra></extra>'
+        hovertemplate='%{hovertext}<extra></extra>',
+        text=[len(Noun[word]) for word in words],  # Число над столбцом
+        textposition='auto'
     ))
 
     # Причастия
     fig.add_trace(go.Bar(
-        x=words,
+        x=words_with_counts,
         y=[len(Participle[word]) for word in words],
         name='Причастия',
         marker_color='blue',
         customdata=[filt_Participle[word] for word in words],
         hovertext=tooltips['Причастия'],
-        hovertemplate='%{hovertext}<extra></extra>'
+        hovertemplate='%{hovertext}<extra></extra>',
+        text=[len(Participle[word]) for word in words],  # Число над столбцом
+        textposition='auto'
     ))
 
     fig.update_layout(
@@ -177,7 +163,6 @@ def create_figure():
 
     return fig
 
-
 # Layout приложения
 app.layout = html.Div([
     dcc.Graph(
@@ -186,7 +171,6 @@ app.layout = html.Div([
     ),
     html.Div(id='output-text')
 ])
-
 
 # Обработка клика по графику
 @app.callback(
@@ -197,21 +181,27 @@ def display_click_data(clickData):
     if clickData is None:
         return "Кликните на столбец, чтобы увидеть список зависимых слов."
 
-    # Получаем категорию и ключевое слово
-    point = clickData['points'][1]
-    label = point['label']  # Ключевое слово
-    category = point['curveNumber']  # Номер категории (0: Sub, 1: Obj, 2: Adj, 3: Noun, 4: Participle)
+    # Получаем данные клика
+    point = clickData['points'][0]
+    word = point['x'].split(' [')[0]  # Извлекаем только ключевое слово без частоты
+    category = point['data']['name']  # Категория
 
-    # Определение категории
-    categories = [filt_Sub, filt_Obj, filt_Adj, filt_Noun, filt_Participle]
-    category_names = ['Глаголы подлежащего', 'Глаголы дополнения', 'Прилагательные', 'Существительные', 'Причастия']
+    # Определение категории и соответствующих данных
+    category_map = {
+        'Глаголы подлежащего': filt_Sub,
+        'Глаголы дополнения': filt_Obj,
+        'Прилагательные': filt_Adj,
+        'Существительные': filt_Noun,
+        'Причастия': filt_Participle
+    }
 
-    # Получаем соответствующие данные
-    data = categories[category][label]
+    data = category_map.get(category, {}).get(word, [])
 
     # Возвращаем список зависимых слов
-    return f"Категория: {category_names[category]}, Ключевое слово: {label}, Список слов: {', '.join(data)}"
-
+    if data:
+        return f"Категория: {category}, Ключевое слово: {word}, Список слов: {', '.join(data)}"
+    else:
+        return "Нет данных для выбранного элемента."
 
 if __name__ == '__main__':
     app.run_server(debug=True)
